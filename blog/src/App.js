2aba579894008f6f288d7651ecf19f19f516dc6e
bys96 <jkpp0611@gmail.com>
2026-02-25 23:44:51 +0900

    state

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,16 +3,35 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  let post = "강남 우동 맛집";
-  let [a, setA] = useState("남자 코트 추천");
+  let [title, setTitle] = useState([
+    "남자 코트 추천",
+    "강남 우동맛집",
+    "파이썬독학",
+  ]);
+
+  let [like, setLike] = useState([0, 0, 0]);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{ backgroundColor: "red" }}>블로그임</h4>
+        <h4 style={{ backgroundColor: "grey" }}>ReactBlog</h4>
       </div>
       <div className="list">
-        <h4>{a}</h4>
+        <h4>
+          {title[0]} <span>👍</span> {like[0]}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>
+          {title[1]} <span>👍</span> {like[1]}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>
+          {title[2]} <span>👍</span> {like[2]}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
